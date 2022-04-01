3f7c0527973961159da334faca9d46d988764e48
APalazzetti <alexpalazzetti@gmail.com>
2022-04-01 15:49:15 +0200

    primo commit

---
+++ src/tcpconnection/Server.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcpconnection;
+import java.net.*;
+/**
+ *
+ * @author alexp
+ */
+public class Server {
+
+     ServerSocket serverSocket= new ServerSocket(2000);
+}
+   
+   
+
+    
+    
+

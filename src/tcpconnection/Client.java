f7639bf2fae9369f6eec7c9179865ca1b984e633
APalazzetti <alexpalazzetti@gmail.com>
2022-04-08 15:41:17 +0200

    aggiunta BufferedWriter

--- src/tcpconnection/Client.java
+++ src/tcpconnection/Client.java
@@ -7,30 +7,56 @@ package tcpconnection;
 import java.io.*;
 import java.net.*;
 
+
 /**
  *
  * @author alexp
  */
 public class Client {
    
-    String messRi;
-    public static void main(String[] argv)
-	    {
-	        try
-	        {
-	            Socket socket = new Socket("127.0.0.1",2000);
-                    BufferedReader  dServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    DataOutputStream VServer = new DataOutputStream(socket.getOutputStream());
-                    String messRi = dServer.readLine();
-                     System.out.println("Il server comunica: "+messRi);
-                     
-                    socket.close();
-                    
-	            
-	        }
-	        catch(IOException ex)
-	        {
-	            System.err.println("porta occupata");
-	        }
-	    }
+
+
+    
+    public static void main(String[] args){
+        
+        try {
+         
+            
+             // creazione socket client            
+            Socket client = new Socket(InetAddress.getLocalHost(),2000);
+             
+             
+        BufferedWriter output;
+          BufferedReader input;
+             
+             output= new BufferedWriter(new OutputStreamWriter (client.getOutputStream()));
+             
+            
+            input= new BufferedReader( new InputStreamReader(client.getInputStream()));
+            
+            String stringaInArr;
+            stringaInArr=input.readLine();
+                      System.out.println("messaggio in arrivo dal server " +stringaInArr+"\n");
+                      // richiesta data e ora
+                    String messaggioIn="inviami data e ora";
+         output.write(messaggioIn+"\r\n");
+              
+              output.flush();
+            
+            stringaInArr=input.readLine();
+                      System.out.println("data ottenuta " +stringaInArr+"\n");
+              
+              
+                client.close();
+                System.out.println("chiusura connessione \n");
+        }
+       
+        
+    
+    
+           catch (Exception ex) {
+          System.out.println("connection error \n");
+            }
+    }
+
 }
--- src/tcpconnection/Server.java
+++ src/tcpconnection/Server.java
@@ -8,6 +8,8 @@ import java.net.*;
 import java.io.*;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 /**
  *
  * @author alexp
@@ -18,29 +20,68 @@ public class Server {
     public static void main(String[] argv)
 	    {
                 
-	        try
-	        {
-                    
-	            ServerSocket serverSocket=new ServerSocket(2000);
-                    System.out.println("server avviato correttamente");
-                    Socket socket=serverSocket.accept();
-                   BufferedReader dClient =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                   DataOutputStream vClient = new DataOutputStream(socket.getOutputStream());
-                   String messInv = "Ben arrivato!";
+	        try {
+          
+               
+          
+       ServerSocket serverSocket = new ServerSocket(2000);
+       
+  System.out.println("server avviato \n");
+             
+             
+       Socket socket=serverSocket.accept();
+         
+         
+         
+        
+        
+         
+             BufferedWriter output;
+          BufferedReader input;    
+             
+             output= new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             
+             
+            input= new BufferedReader( new InputStreamReader(socket.getInputStream()));
+            
+            String messaggioIn="Benvenuto";
+            
+            
+            
+         output.write(messaggioIn+"\r\n");
+             
+           output.flush();
+            
+                
+            String stringaInArr;
+            stringaInArr=input.readLine();
+                      System.out.println("richiesta " +stringaInArr+"\n");
+                      
+                      
+                      
+                      SimpleDateFormat data = new SimpleDateFormat("yyyy.MM.dd.HH:mm:ss");
+        String dataOra = data.format(new Date());
                    
-                    socket.close();
-	            
-	        }
-	        catch(IOException ex)
-	        {
-	            System.err.println("porta occupata");
-	        }
-	    }
-    private BufferedReader dalClient;
+         output.write(dataOra+"\r\n");
+              
+            output.flush();
+              
+        
+         socket.close();
+           System.out.println("connection close \n");
+            serverSocket.close();
+    }
+        catch (IOException ex) {
+            System.out.println("Errore IO");
+        }
+        
+    }
+    
+}
 
 
      
-}
+
    
    
 

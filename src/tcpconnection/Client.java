7e0afffd080d2be31eee54a795ad78cd2556b0e3
APalazzetti <alexpalazzetti@gmail.com>
2022-04-08 08:47:22 +0200

    punto 2

--- src/tcpconnection/Client.java
+++ src/tcpconnection/Client.java
@@ -4,11 +4,33 @@
  * and open the template in the editor.
  */
 package tcpconnection;
+import java.io.*;
+import java.net.*;
 
 /**
  *
  * @author alexp
  */
 public class Client {
-    
+   
+    String messRi;
+    public static void main(String[] argv)
+	    {
+	        try
+	        {
+	            Socket socket = new Socket("127.0.0.1",2000);
+                    BufferedReader  dServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    DataOutputStream VServer = new DataOutputStream(socket.getOutputStream());
+                    String messRi = dServer.readLine();
+                     System.out.println("Il server comunica: "+messRi);
+                     
+                    socket.close();
+                    
+	            
+	        }
+	        catch(IOException ex)
+	        {
+	            System.err.println("porta occupata");
+	        }
+	    }
 }
--- src/tcpconnection/Server.java
+++ src/tcpconnection/Server.java
@@ -5,13 +5,41 @@
  */
 package tcpconnection;
 import java.net.*;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 /**
  *
  * @author alexp
  */
 public class Server {
+    
 
-     ServerSocket serverSocket= new ServerSocket(2000);
+    public static void main(String[] argv)
+	    {
+                
+	        try
+	        {
+                    
+	            ServerSocket serverSocket=new ServerSocket(2000);
+                    System.out.println("server avviato correttamente");
+                    Socket socket=serverSocket.accept();
+                   BufferedReader dClient =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                   DataOutputStream vClient = new DataOutputStream(socket.getOutputStream());
+                   String messInv = "Ben arrivato!";
+                   
+                    socket.close();
+	            
+	        }
+	        catch(IOException ex)
+	        {
+	            System.err.println("porta occupata");
+	        }
+	    }
+    private BufferedReader dalClient;
+
+
+     
 }
    
    
